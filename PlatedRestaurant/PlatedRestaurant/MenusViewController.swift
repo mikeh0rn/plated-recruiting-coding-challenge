1ef48f567dc591a641f3271e3d9fb62c4a2d6ad1
mikeh0rn <mikelaurencehorn@gmail.com>
2018-11-26 14:38:31 -0500

    setup test tableview with dummy data

--- PlatedRestaurant/PlatedRestaurant/MenusViewController.swift
+++ PlatedRestaurant/PlatedRestaurant/MenusViewController.swift
@@ -8,15 +8,35 @@
 
 import UIKit
 
-class MenusViewController: UIViewController {
+class MenusViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var menusArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        menusArray.append("Italian")
+        menusArray.append("Chinese")
+        menusArray.append("Mexican")
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menusArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = menusArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     /*
     // MARK: - Navigation
 
