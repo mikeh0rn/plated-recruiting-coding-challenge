f7c1b0b1b853884495de9db5ce3b5c737f46b801
mikeh0rn <mikelaurencehorn@gmail.com>
2018-11-26 13:53:17 -0500

    first commit - create project

---
+++ PlatedRestaurant/PlatedRestaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlatedRestaurant
+//
+//  Created by Mike Horn on 11/26/18.
+//  Copyright © 2018 Mike Horn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
