9b4ce31124cf847251672ecb020c20d3d89ee41c
mikeh0rn <mikelaurencehorn@gmail.com>
2018-11-26 14:50:45 -0500

    more dummy data in prepare for segue

--- PlatedRestaurant/PlatedRestaurant/MenusViewController.swift
+++ PlatedRestaurant/PlatedRestaurant/MenusViewController.swift
@@ -36,15 +36,13 @@ class MenusViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: "", sender: menusArray[indexPath.row])
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+ 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let recipesOnMenuVC = segue.destination as! RecipesOnMenuViewController
+        recipesOnMenuVC.stringToPass = sender
     }
-    */
+ 
 
 }
