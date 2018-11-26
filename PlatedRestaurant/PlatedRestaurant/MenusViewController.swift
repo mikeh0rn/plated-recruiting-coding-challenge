97429789cc2b6ba421e9ac08ec4a08ca923165ab
mikeh0rn <mikelaurencehorn@gmail.com>
2018-11-26 14:28:54 -0500

    add cocoa touch classes and assign them to each VC in storyboard

---
+++ PlatedRestaurant/PlatedRestaurant/MenusViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenusViewController.swift
+//  PlatedRestaurant
+//
+//  Created by Mike Horn on 11/26/18.
+//  Copyright © 2018 Mike Horn. All rights reserved.
+//
+
+import UIKit
+
+class MenusViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PlatedRestaurant/PlatedRestaurant/RecipeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeDetailViewController.swift
+//  PlatedRestaurant
+//
+//  Created by Mike Horn on 11/26/18.
+//  Copyright © 2018 Mike Horn. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
