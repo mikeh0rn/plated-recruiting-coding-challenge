f37aace4b1ab0d6c277bd10e2f45343b4b72328e
mikeh0rn <mikelaurencehorn@gmail.com>
2018-11-27 11:08:58 -0500

    use activity indicator, only call API if titles arent already set on BACK

--- PlatedRestaurant/PlatedRestaurant/MenusViewController.swift
+++ PlatedRestaurant/PlatedRestaurant/MenusViewController.swift
@@ -12,35 +12,43 @@ class MenusViewController: UIViewController {
     
     @IBOutlet weak var topButton: UIButton!
     @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var menusArray = [MenuViewModel]()
     var apiManager = APIManager.sharedInstance
     
     override func viewDidAppear(_ animated: Bool) {
-        apiManager.menus(completion: { result in
-            if result.count > 0 {
-                for item in result {
-                    self.menusArray.append(item)
-                    if self.menusArray.count == 2 {
-                        self.populateButtonData()
+        super.viewDidAppear(animated)
+        if topButton.currentTitle == nil || bottomButton.currentTitle == nil {
+            self.activityIndicator.startAnimating()
+            apiManager.menus(completion: { result in
+                if result.count > 0 {
+                    for item in result {
+                        self.menusArray.append(item)
+                        if self.menusArray.count == 2 {
+                            self.populateButtonWithData()
+                        }
                     }
                 }
-            }
-        })
+            })
+        }
+
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    func populateButtonData() {
+    func populateButtonWithData() {
         self.topButton.setTitle(self.menusArray[0].title, for: UIControl.State.normal)
         self.topButton.tag = self.menusArray[0].id ?? 0
         
         self.bottomButton.setTitle(self.menusArray[1].title, for: UIControl.State.normal)
         self.bottomButton.tag = self.menusArray[1].id ?? 0
+        self.activityIndicator.stopAnimating()
     }
     
+
     @IBAction func buttonSelected(_ sender: UIButton) {
         performSegue(withIdentifier: "segueToRecipesOnMenu", sender: sender.tag)
     }
