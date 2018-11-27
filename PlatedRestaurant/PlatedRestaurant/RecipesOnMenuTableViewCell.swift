22453033f7a23b78f6ee94c8e947357a96f072dc
mikeh0rn <mikelaurencehorn@gmail.com>
2018-11-26 21:24:47 -0500

    add custom Table View Cell to connect to Recipes on Menus View Controller

---
+++ PlatedRestaurant/PlatedRestaurant/RecipesOnMenuTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  RecipesOnMenuTableViewCell.swift
+//  PlatedRestaurant
+//
+//  Created by Mike Horn on 11/26/18.
+//  Copyright © 2018 Mike Horn. All rights reserved.
+//
+
+import UIKit
+
+class RecipesOnMenuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var recipeDescription: UILabel!
+    @IBOutlet weak var recipeName: UILabel!
+    @IBOutlet weak var recipeImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
